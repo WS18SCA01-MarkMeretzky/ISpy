b112015bd17f6627b0a64f6dd7af32a2da5f1e12
Mark Meretzky <mark.meretzky@gmail.com>
2019-02-02 00:00:13 -0500

    Commit.

--- ISpy/ViewController.swift
+++ ISpy/ViewController.swift
@@ -6,15 +6,47 @@
 //  Copyright © 2019 New York University School of Professional Studies. All rights reserved.
 //
 
-import UIKit
+import UIKit;
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!;
+    @IBOutlet weak var imageView: UIImageView!;
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
+        scrollView.delegate = self;
+        updateZoomFor(size: view.frame.size);
+    }
+    
+    //Called when the scrollView has been given its correct size.
+    
+    override func viewDidLayoutSubviews() {
+        //Zoom far enough out to see the entire image.
+        scrollView.setZoomScale(scrollView.minimumZoomScale, animated: false);
+    }
+    
+    //Let the user zoom far enough out to see the entire image, but no farther.
+
+    func updateZoomFor(size: CGSize) {
+        let widthScale: CGFloat = size.width / imageView.frame.width;
+        let heightScale: CGFloat = size.height / imageView.frame.height;
+        scrollView.minimumZoomScale = min(widthScale, heightScale);
+    }
+    
+    //MARK: UIScrollViewDelegate
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView;
+    }
+    
+    //If the content does not fill the entore scrollview, center the content.
+
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let top:  CGFloat = max((scrollView.frame.height - scrollView.contentSize.height) / 2, 0);
+        let left: CGFloat = max((scrollView.frame.width  - scrollView.contentSize.width)  / 2, 0);
+        scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: 0, right: 0);
     }
 
-
 }
-
